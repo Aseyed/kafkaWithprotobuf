ac26a2d4508abe9085fe122573c5e96d272936f5
Aseyed Mostafa <hosseini.m@mail.randmail.yaftar.ir>
2020-02-19 14:29:36 +0330

    modigy producer

--- producer.py
+++ producer.py
@@ -1,8 +1,9 @@
 from confluent_kafka import Producer
-from protobuf.data.ImageData_pb2 import Image
+from protobuf.data.data_pb2 import Msg
 import socket
 import sys
 import logging
+import base64
 
 logging.basicConfig(level=logging.DEBUG)
 conf = {'bootstrap.servers': "localhost:9092"}
@@ -16,17 +17,23 @@ def acked(err, msg):
         sys.stderr.write('%% Message delivered to %s [%d] @ %d\n' %
                             (msg.topic(), msg.partition(), msg.offset()))
 
+image = open("img.png", "rb")
+f = image.read()
+image.close()
+# myBytes = bytearray(f)
 
-test_image = Image(
-    image_data = b"this is just a test string",
-    height = 10,
-    width = 10
+my_message = Msg(
+    data = f,
+    app_name = 'img',
+    file_ext = 'png'
 )
-for frame in range(100):
-    test_image.frame = frame
+
+for frame in range(10):
+    my_message.frame = frame
     # producer.send(test_image)
-    myObject = test_image.SerializeToString()
-    producer.produce('VideoStream',myObject, callback=acked)
+    myObject = base64.b64encode(my_message.SerializeToString())
+    res = producer.produce('VideoStream',myObject, callback=acked)
+    print(res)
     
     # Wait up to 1 second for events. Callbacks will be invoked during
     # this method call if the message is acknowledged.
