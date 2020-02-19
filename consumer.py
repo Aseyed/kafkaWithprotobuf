80b2fe6b36d8ffd58e25bbe56bc42d82a593c860
Aseyed Mostafa <hosseini.m@mail.randmail.yaftar.ir>
2020-02-19 14:29:48 +0330

    modify consumer

--- consumer.py
+++ consumer.py
@@ -1,6 +1,7 @@
 from confluent_kafka import Consumer, KafkaException, KafkaError
-from protobuf.data.ImageData_pb2 import Image
+from protobuf.data.data_pb2 import Msg
 import sys
+import base64
 
 conf = {'bootstrap.servers': "localhost:9092",
         'auto.offset.reset': 'smallest',
@@ -10,23 +11,31 @@ consumer = Consumer(conf)
 
 running = True
 
+theMessage = Msg()
 try:
     consumer.subscribe(['VideoStream'])
 
+    i = 0
     while running:
         msg = consumer.poll(timeout=1.0)
         if msg is None: continue
 
         if msg.error():
             if msg.error().code() == KafkaError._PARTITION_EOF:
-                    # End of partition event
+                # End of partition event
                 sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
                                                 (msg.topic(), msg.partition(), msg.offset()))
             elif msg.error():
                 raise KafkaException(msg.error())
         else:
-            print(msg.value().decode('ascii').split('\n')[1])
+            i += 1
+            # print(base64.b64decode(msg.value()))
+            theMessage.ParseFromString(base64.b64decode(msg.value()))
+            f = open(f'/tmp/{theMessage.app_name}_{i}.{theMessage.file_ext}', 'wb')
+            f.write(theMessage.data)
+            f.close()
+#            break
 finally:
 # Close down consumer to commit final offsets.
     consumer.close()
-        
+
